5e9edb19884ec2de49c133f4f5e47fff0358d4d5
GoatedChopin <85979611+GoatedChopin@users.noreply.github.com>
2021-12-05 18:50:15 -0700

    Add files via upload

--- AdventOfCode/src/Day3_Diagnostic.java
+++ AdventOfCode/src/Day3_Diagnostic.java
@@ -24,7 +24,7 @@ public class Day3_Diagnostic {
         int zeroCount = 0;
         int oneCount = 0;
         for (int i = 0; i < dataList.size(); i++) {
-            if (dataList.get(i).substring(bitPosition, bitPosition+1).equals("0")) zeroCount++;
+            if (dataList.get(i).charAt(bitPosition) == '0') zeroCount++;
             else oneCount++;
         }
         System.out.println("There were " + zeroCount +" zeros and " + oneCount + " ones");
@@ -50,13 +50,33 @@ public class Day3_Diagnostic {
         return Integer.parseInt(outputString, 2);
     }
 
-    public static int filterFindRating(ArrayList<Integer> arrayList, boolean useMax) {
+    public static ArrayList<String> filterFindRating(int bitPosition, ArrayList<String> arrayList, boolean useMax) {
+        String mode = Integer.toString(findModeOfPosition2(bitPosition, arrayList));
+        ArrayList<Integer> removalList = new ArrayList();
         if (useMax) {
-
+            for (int i = 0; i < arrayList.size(); i++) if (!arrayList.get(i).substring(bitPosition, bitPosition + 1).equals(mode)) removalList.add(i);
         } else {
-
+            for (int w = 0; w < arrayList.size(); w++) if (arrayList.get(w).substring(bitPosition,bitPosition+1).equals(mode)) removalList.add(w);
         }
+        arrayList.removeAll(removalList);
+        return arrayList;
+    }
 
+    public static int findModeOfPosition2(int bitPosition, ArrayList<String> dataList) {
+        int zeroCount = 0;
+        int oneCount = 0;
+        for (int i = 0; i < dataList.size(); i++) {
+            if (dataList.get(i).charAt(bitPosition) == '0') zeroCount++;
+            else oneCount++;
+        }
+        System.out.println("There were " + zeroCount +" zeros and " + oneCount + " ones");
+
+        if (zeroCount > oneCount) {
+            return 0;
+        }
+        else {
+            return 1;
+        }
     }
 
     public static void main(String[] args) {
@@ -68,6 +88,32 @@ public class Day3_Diagnostic {
         int gammaInt = convertListToDecimal(gamma);
         int epsilonInt = convertListToDecimal(epsilon);
         System.out.println(gammaInt * epsilonInt);
-    }
 
+        for (int i = 0; i < dataList.get(0).length(); i++) {
+            if (dataList.size() > 1) {
+                dataList = filterFindRating(i, dataList, true);
+            }
+        }
+        String oxygenGeneratorRating = dataList.get(0);
+        System.out.println("Oxygen Generator Rating = " + oxygenGeneratorRating);
+        System.out.println(Integer.parseInt(oxygenGeneratorRating, 2));
+        dataList.clear();
+        try (Scanner scanner = new Scanner(new FileReader(dataFile))) {
+            while (scanner.hasNext()) {
+                dataList.add(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < dataList.get(0).length(); i++) {
+            if (dataList.size() > 1) {
+                dataList = filterFindRating(i, dataList, false);
+            }
+        }
+        String co2ScrubberRating = dataList.get(0);
+        System.out.println("CO2 Scrubber Rating = " + co2ScrubberRating);
+        System.out.println(Integer.parseInt(co2ScrubberRating, 2));
+        // Answer Incorrect, needs revision
+        System.out.println(Integer.parseInt(co2ScrubberRating, 2) * Integer.parseInt(oxygenGeneratorRating, 2));
+    }
 }
